8d2c58721276592b92b0f59b9204e8d1d3b72145
NeoremFf <maksim.kedrovskiy@gmail.com>
2020-08-12 13:27:07 +0300

    Added main core

---
+++ BankApplication/BankLib/DepositAccount.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankLib
+{
+    public class DepositAccount : Account
+    {
+        public DepositAccount(decimal sum, int percentage) : base(sum, percentage)
+        {
+        }
+
+        protected internal override void Open()
+        {
+            base.OnOpened(new AccountEventArgs($"Открыт новый депозитный счет! Id счета: {this.Id}", this.Sum));
+        }
+
+        public override void Put(decimal sum)
+        {
+            if (IsRightDay())
+                base.Put(sum);
+            else
+                base.OnAdded(new AccountEventArgs("На счет можно положить только после 30-ти дневного периода", 0));
+        }
+
+        public override decimal Withdraw(decimal sum)
+        {
+            if (IsRightDay())
+                return base.Withdraw(sum);
+            else
+                base.OnWithdrawed(new AccountEventArgs("Вывести средства можно только после 30-ти дневного периода", 0));
+            return 0;
+        }
+
+        protected internal override void Calculate()
+        {
+            if (_days % 30 == 0)
+                base.Calculate();
+        }
+
+        private bool IsRightDay()
+        {
+            return _days > 30;
+        }
+    }
+}
